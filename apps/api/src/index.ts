26dc9ed8caab14f5751a9f2fa86adb2094ea6191
Karl J. Overå <k@rl.run>
2024-05-22 11:45:27 +0200

    chore: deploy api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,2 +1,11 @@
-// eslint-disable-next-line no-console
-console.log('Hello!')
+import { Elysia } from 'elysia'
+
+new Elysia()
+    .get('/', () => 'Hello Elysia')
+    .get('/arbeidsforhold', ({ headers }) => headers['X-Fnr'])
+    .get('/internal/is_alive', () => ({ status: 'ok' }))
+    .get('/internal/is_ready', () => ({ status: 'ok' }))
+    .onStart(() => {
+        console.log(`Server started on port ${Bun.env.PORT ?? 3000}`)
+    })
+    .listen(Bun.env.PORT ?? 3000)
